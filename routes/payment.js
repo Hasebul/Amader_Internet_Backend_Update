91765d3bfe3ac13eaf916d306aacde161b19f6be
Shawon <hasebulhasan97@gmail.com>
2021-07-26 00:16:18 +0600

    change in isp controller

--- routes/payment.js
+++ routes/payment.js
@@ -9,5 +9,9 @@ const paymentController = require('../controllers/paymentController');
 
 paymentRouter.post('/insert',paymentController.handlePaymentInsertOne );//ispController.handleIspInsertOne
 paymentRouter.post('/fetchAllIspPayment',paymentController.handleFetchAllIspPayment);
+paymentRouter.post('/fetchIspOwnPayment',paymentController.handleFetchIspOwnPayment);
+paymentRouter.post('/fetchAllUserOfIspPayment',paymentController.handleFetchAllUserOfIspPayment);
+paymentRouter.post('/fetchUserOwnPayment',paymentController.handleFetchUserOwnPayment);
+
 
 module.exports = paymentRouter ;
