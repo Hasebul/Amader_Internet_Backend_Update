92d6c47ce1f4a882a1150da5fe02238332ebfdd0
Shawon <hasebulhasan97@gmail.com>
2021-07-25 20:36:20 +0600

    change in isp controller

--- db/model/Notification.js
+++ db/model/Notification.js
@@ -32,8 +32,6 @@ var NotificationSchema = new mongoose.Schema({
     
     details: {
         type : String,
-        trim : true,
-        minlength : 5,
         required : true
     },
     seenStatus : {
